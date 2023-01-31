1e72ab997b27fe9e7ed014869d5c01d38720198e
Volodymyr Pasichnyi <vlpasnj33@gmail.com>
2023-01-31 13:43:33 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Toaster } from "react-hot-toast"
 import { Route, Routes } from "react-router-dom"
-
+import { Layout } from "./Layout/Layout";
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>Home</div>}></Route>
+        <Route path="/" element={<Layout />}></Route>
       </Routes>
       <Toaster position="top-right" reverseOrder={false}/>
     </>
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import css from '../Layout/Layout.module.css'
+
+
+export const Layout = () => {
+    return (
+        <div className={css.layout}>
+            <p>Home</p>
+            <p>Movies</p>
+        </div>
+    )
+}
