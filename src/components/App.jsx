449dfe8be6aa9214683479e57176ad4a7211ebc3
Volodymyr Pasichnyi <vlpasnj33@gmail.com>
2023-02-01 11:03:58 +0200

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,22 @@ import { Routes, Route } from "react-router-dom"
 import { Layout } from "./Layout/Layout";
 import { Home } from "pages/Home/Home";
 import { Movies } from "pages/Movies/Movies";
+import { MovieDetails } from "./MovieDetails/MovieDetails";
 
 
 export const App = () => {
   return (
-    <>
+    <div>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home/>}/>
           <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails/>}>
 
+          </Route>
         </Route>
       </Routes>
       <Toaster position="top-right" reverseOrder={false}/>
-    </>
+    </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,44 @@
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { trendingMovieApi } from 'services/moviesApi'
+import { Link, Outlet } from 'react-router-dom'
+import { Suspense } from 'react'
 
 
 export const Home = () => {
-    const [trending, useTrending] = useState([])
+    const [movies, setMovies] = useState([])
 
     useEffect(() => {
         trendingMovieApi()
-          .then(useTrending)
+          .then(response => {
+            if (response.ok) {
+              return response.json()
+            }
+            return Promise.reject(new Error('Not found'))
+          })
+          .then(data => setMovies(data.results))
+          .catch(error => console.log(error))
     }, [])
 
-    if (!trending) {
-        return 'Not Found'
-      }
+    if (!movies) {
+        return 
+    }
 
     return (
-        <>
-        <h2>Trending Today</h2>
-        </>
+      <>
+        <div>
+          <h2>Trending Today:</h2>
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>
+                {movie.title || movie.name}
+              </Link>
+            </li>
+        ))}
+        </div>
+        {/* <Suspense> */}
+          <Outlet/>
+        {/* </Suspense> */}
+      </>
     )
 }
